59a76230962baaf1063dff91a96f09856e788226
siminski <laurensiminski@gmail.com>
2020-08-15 10:09:01 -0400

    seed file broken into seed and schema

--- seed.sql
+++ seed.sql
@@ -1,27 +1,11 @@
-DROP DATABASE IF EXISTS employeeDB;
-CREATE DATABASE employeeDB;
+INSERT INTO department (name)
+VALUES ('Design'), ('Marketing');
 
-USE employeeDB;
+INSERT INTO role (title, salary, departmentId)
+VALUES ('Design Director', 90000, 1), ('Web Designer', 50000, 1), ('Print Designer', 40000, 1), ('Creative Director', 80000, 2), ('Marketing Manager', 70000, 2), ('Social Media Specialist', 40000, 2);
 
-CREATE TABLE department (
-	id INT NOT NULL AUTO_INCREMENT,
-    name VARCHAR(30) NOT NULL,
-    PRIMARY KEY (id)
-);
+INSERT INTO employee (firstName, lastName, roleId)
+VALUES ('Joseph', 'Carter Brown', 1), ('Megan', 'Johnson', 4), ('Stacy', 'Ruddy', 5);
 
-CREATE TABLE role (
-	id INT NOT NULL AUTO_INCREMENT,
-    title VARCHAR(30) NOT NULL,
-    salary INT NOT NULL,
-    departmentId INT NOT NULL,
-    PRIMARY KEY (id)
-);
-
-CREATE TABLE employee (
-	id INT NOT NULL AUTO_INCREMENT,
-    firstName VARCHAR(30) NOT NULL,
-    lastName VARCHAR(30) NOT NULL,
-    roleId INT NOT NULL,
-    managerId INT,
-    PRIMARY KEY (id)
-);
+INSERT INTO employee (firstName, lastName, roleId, managerId)
+VALUES ('Lauren', 'Siminski', 2, 2), ('Jessica', 'Meyer', 3, 1), ('Rory', 'Nachbar', 6, 3);
